d987b2e6eb36a95b97f85fbd4b37f66cee0a267f
Joseph Huskey <josephhuskey@Josephs-MacBook-Air.local>
2020-10-01 15:59:42 -0700

    Update VectorsTutorial.sln, Program.cs, and 3 more files...

---
+++ VectorsTutorial/VectorsTutorial/Vector2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace VectorsTutorial
+{
+    class Vector2
+    {
+        public float x, y; 
+    }
+
+    class Vector3
+    { 
+        public float x, y, z;
+        private float v1;
+        private float v2;
+        private float v3;
+
+        public Vector3(float v1, float v2, float v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+
+        public static Vector3 operator +(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+        }
+        public static Vector3 operator -(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+        }
+    }
+}
