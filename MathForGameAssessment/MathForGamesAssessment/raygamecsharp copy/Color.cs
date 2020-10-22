fbc2d614dc2065c46063ee84c82c54425b37a806
Joseph Huskey <josephhuskey@Josephs-MacBook-Air.local>
2020-10-22 11:39:06 -0700

    Did the timer tutorial and changed Color to Colour in the color class for the test

--- MathForGameAssessment/MathForGamesAssessment/raygamecsharp copy/Color.cs
+++ MathForGameAssessment/MathForGamesAssessment/raygamecsharp copy/Color.cs
@@ -1,54 +1,54 @@
 ﻿using System;
 namespace VectorsTutorial
 {
-    public class Color
+    public class Colour
     {
-        public UInt32 color;
-        public Color()
+        public UInt32 colour;
+        public Colour()
         {
         }
-        public Color(byte red, byte green, byte blue, byte alpha)
+        public Colour(byte red, byte green, byte blue, byte alpha)
         { }
         //getting and setting rgb and alpha.
         public byte GetRed()
         {
-            UInt32 value = color & 0xff000000;
+            UInt32 value = colour & 0xff000000;
             return (byte)((value) >> 24);
         }
         public void SetRed(byte red)
         {
-            color = color & 0x00ffffff;
-            color |= (UInt32)red << 24;
+            colour = colour & 0x00ffffff;
+            colour |= (UInt32)red << 24;
         }
         public byte GetGreen()
         {
-            UInt32 value = color & 0xff000000;
+            UInt32 value = colour & 0xff000000;
             return (byte)((value) >> 24);
         }
         public void SetGreen(byte green)
         {
-            color = color & 0x00ffffff;
-            color |= (UInt32)green << 24;
+            colour = colour & 0x00ffffff;
+            colour |= (UInt32)green << 24;
         }
         public byte GetBlue()
         {
-            UInt32 value = color & 0xff000000;
+            UInt32 value = colour & 0xff000000;
             return (byte)((value) >> 24);
         }
         public void SetBlue(byte blue)
         {
-            color = color & 0x00ffffff;
-            color |= (UInt32)blue << 24;
+            colour = colour & 0x00ffffff;
+            colour |= (UInt32)blue << 24;
         }
         public byte GetAlpha()
         {
-            UInt32 value = color & 0xff000000;
+            UInt32 value = colour & 0xff000000;
             return (byte)((value) >> 24);
         }
         public void SetAlpha(byte alpha)
         {
-            color = color & 0x00ffffff;
-            color |= (UInt32)alpha << 24;
+            colour = colour & 0x00ffffff;
+            colour |= (UInt32)alpha << 24;
         }
     }
 }
